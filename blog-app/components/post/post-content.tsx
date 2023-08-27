1aa069e98ab2ad2e541ac4c7d17cf9cdaa16d8d7
orkunkhrmn <orkunkahraman@gmail.com>
2023-08-27 19:57:34 +0300

    add reading-time and luxon package for read time and prety date

--- blog-app/components/post/post-content.tsx
+++ blog-app/components/post/post-content.tsx
@@ -1,3 +1,4 @@
+import { getReadingTime, getRelativeDate } from "@/lib/helpers";
 import { Post } from "@/types/collection";
 import { ArrowRight } from "lucide-react";
 import React from "react";
@@ -23,9 +24,9 @@ const PostContent = ({ post }: PostContentProps) => {
         <div className="w-2 h-2 rounded-full bg-neutral-200" />
         <div>{`${post.author.first_name} ${post.author.last_name}`}</div>
         <div className="w-2 h-2 rounded-full bg-neutral-200" />
-        <div>1 min read</div>
+        <div>{getReadingTime(post.body)}</div>
         <div className="w-2 h-2 rounded-full bg-neutral-200" />
-        <div>12 month ago</div>
+        <div>{getRelativeDate(post.date_created)}</div>
       </div>
       {/* title */}
       <h2 className="font-medium text-3xl">{post.title}</h2>
