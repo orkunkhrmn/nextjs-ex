dc6546422c00067f3a4e88f86f73d5c7979416e3
orkunkhrmn <orkunkahraman@gmail.com>
2023-08-27 19:49:40 +0300

    added post list

--- blog-app/app/page.tsx
+++ blog-app/app/page.tsx
@@ -1,5 +1,6 @@
 import PaddingContainer from "@/components/layout/padding-container";
 import PostCard from "@/components/post/post-card";
+import PostList from "@/components/post/post-list";
 import { DUMMY_POSTS } from "@/DUMMY_DATA";
 
 export default function Home() {
@@ -7,6 +8,9 @@ export default function Home() {
     <PaddingContainer>
       <main className="h-auto space-y-10">
         <PostCard post={DUMMY_POSTS[0]} />
+        <PostList
+          posts={DUMMY_POSTS.filter((_post, index) => index > 0 && index < 3)}
+        />
       </main>
     </PaddingContainer>
   );
