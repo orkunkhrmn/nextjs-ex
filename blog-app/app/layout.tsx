5d5afecdcd87385fd95a0a0d8e8023860192ed98
orkunkhrmn <orkunkahraman@gmail.com>
2023-08-27 13:53:01 +0300

    change home

--- blog-app/app/layout.tsx
+++ blog-app/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navigation />
-        {children}
+        <div className="pt-10">{children}</div>
         <Footer />
       </body>
     </html>
--- blog-app/app/page.tsx
+++ blog-app/app/page.tsx
@@ -1,10 +1,11 @@
+import PaddingContainer from "@/components/layout/padding-container";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="h-[auto]">
-      <h1>Home</h1>
-    </main>
+    <PaddingContainer>
+      <main className="h-auto space-y-10"></main>
+    </PaddingContainer>
   );
 }
